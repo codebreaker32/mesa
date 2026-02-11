fd4605fc8505078b3aeecdc7a489c1a479fc6df2
Aman <anni18938@gmail.com>
2026-02-11 15:10:05 +0000

    Revert "Temp commit for checking benchmarks"
    This reverts commit 6600746c561c85da7f3c806896be0b850319a0ee.

--- mesa/examples/advanced/wolf_sheep/model.py
+++ mesa/examples/advanced/wolf_sheep/model.py
@@ -12,9 +12,9 @@ Replication of the model found in NetLogo:
 import math
 
 from mesa import Model
+from mesa.datacollection import DataCollector
 from mesa.discrete_space import OrthogonalVonNeumannGrid
 from mesa.examples.advanced.wolf_sheep.agents import GrassPatch, Sheep, Wolf
-from mesa.experimental.data_collection import DataRecorder, DatasetConfig
 from mesa.experimental.devs import ABMSimulator
 
 
@@ -77,11 +77,17 @@ class WolfSheep(Model):
             random=self.random,
         )
 
-        model_fields = ["num_wolves", "num_sheep"]
+        # Set up data collection
+        model_reporters = {
+            "Wolves": lambda m: len(m.agents_by_type[Wolf]),
+            "Sheep": lambda m: len(m.agents_by_type[Sheep]),
+        }
         if grass:
-            model_fields.append("num_grass")
+            model_reporters["Grass"] = lambda m: len(
+                m.agents_by_type[GrassPatch].select(lambda a: a.fully_grown)
+            )
 
-        self.data_registry.track_model(self, "model_data", fields=model_fields)
+        self.datacollector = DataCollector(model_reporters)
 
         # Create sheep:
         Sheep.create_agents(
@@ -114,30 +120,13 @@ class WolfSheep(Model):
 
         # Collect initial data
         self.running = True
-        self.datarecorder = DataRecorder(
-            self,
-            config={
-                "model_data": DatasetConfig(interval=1),
-            },
-        )
-
-    @property
-    def num_wolves(self):
-        """Count wolves."""
-        return len(self.agents_by_type[Wolf])
-
-    @property
-    def num_sheep(self):
-        """Count sheep."""
-        return len(self.agents_by_type[Sheep])
-
-    @property
-    def num_grass(self):
-        """Count fully grown grass patches."""
-        return len(self.agents_by_type[GrassPatch].select(lambda a: a.fully_grown))
+        self.datacollector.collect(self)
 
     def step(self):
         """Execute one step of the model."""
         # First activate all sheep, then all wolves, both in random order
         self.agents_by_type[Sheep].shuffle_do("step")
         self.agents_by_type[Wolf].shuffle_do("step")
+
+        # Collect data
+        self.datacollector.collect(self)
